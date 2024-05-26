b627e840b158ee6bc7df6f72a0231d133e096848
Ali Zand <ali.zand9224@gmail.com>
2024-05-25 18:42:52 -0600

    Add initializer

--- Sources/SwiftWhisper/Segment.swift
+++ Sources/SwiftWhisper/Segment.swift
@@ -4,4 +4,10 @@ public struct Segment: Equatable, Codable {
     public let startTime: Int
     public let endTime: Int
     public let text: String
+
+    public init(startTime: Int, endTime: Int, text: String) {
+        self.startTime = startTime
+        self.endTime = endTime
+        self.text = text
+    }
 }
