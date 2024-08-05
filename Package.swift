030c1a96c6da5da6fbf0019bb1b9260cef7ebe0c
Ali Zand <ali.zand9224@gmail.com>
2024-08-04 18:37:58 -0600

    Add optimization

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
                     .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
                     .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
                     .define("WHISPER_COREML_ALLOW_FALLBACK", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                    .unsafeFlags(["-O3"]),
                 ]),
         .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")])
     ],
